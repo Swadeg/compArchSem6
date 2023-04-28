146a80d8aa0357237e3df48326d82556377840db
Swadeg <ali-swade@campus.technion.ac.il>
2023-04-28 15:50:11 +0300

    running version wrong out

--- bp.cpp
+++ bp.cpp
@@ -21,34 +21,53 @@ class branchPredictor {
 	bool isGlobalTable;
 	int isShare; /*0-not_using_share, 1- using_share_lsb, 2- using_share_mid*/
 	SIM_stats stats; 
+	uint32_t global_history;
 	vector<uint32_t> tag_vector;
 	vector<uint32_t> history_vector;
 	vector<uint32_t> target_vector;
-	vector<vector<int>> fsm_table;  
-	uint32_t global_history;
 	vector<int> global_fsm_table;
+	vector<vector<int>> fsm_table;  
 	
 public:
-	branchPredictor();
+	branchPredictor(){};
 	branchPredictor(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
 			bool isGlobalHist, bool isGlobalTable, int Shared);
-	~branchPredictor();
+	~branchPredictor(){};
 	bool predict(uint32_t pc, uint32_t *dst);
 	void update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst);
 	/*add methods here*/
 	uint32_t getSharedHistory(uint32_t pc, uint32_t history);
 	uint32_t getTagFromPc(uint32_t pc);
-	int getTagIdx(uint32_t tag); /*return -1 if tag doesnt exist else returns tagIdx*/
+	int getTagIdx(uint32_t pc, uint32_t tag); /*return -1 if tag doesnt exist else returns tagIdx*/
 	uint32_t getTargetFromTagIdx(int tagIdx); /*assume tag is exist in btb*/
 	void updateTarget(int tagIdx, uint32_t targetPc);
 	void updateFsm(uint32_t pc, int tagIdx, bool taken);
-	void insertNewBranch(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst);
+	void insertNewBranch(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst, int tagIdx, uint32_t tag);
 	void updateHistory( int tagIdx, bool taken);
+	bool isTaken(uint32_t pc, int tagIdx);
 };
 
 
 
 //class methods
+
+branchPredictor::branchPredictor(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
+		bool isGlobalHist, bool isGlobalTable, int Shared)
+{
+	btbSize = btbSize;
+	historySize = historySize;
+	tagSize = tagSize;
+	fsmState = fsmState;
+	isGlobalHist = isGlobalHist;
+	isGlobalTable = isGlobalTable;
+	Shared = Shared;
+	global_history = 0;
+	tag_vector = vector<uint32_t>(btbSize,0);
+	history_vector = vector<uint32_t>(btbSize,0);
+	target_vector = vector<uint32_t>(btbSize,0);
+	global_fsm_table = vector<int>(btbSize,0);
+	fsm_table = vector<vector<int>>(btbSize,vector<int>(pow(2,historySize), fsmState));
+}
 
 uint32_t branchPredictor::getSharedHistory(uint32_t pc, uint32_t history)
 {
@@ -64,6 +83,7 @@ uint32_t branchPredictor::getSharedHistory(uint32_t pc, uint32_t history)
 	{
 		return ((pc>>16)^(history)) & historySize;
 	}
+	return -1;
 }
 
 uint32_t branchPredictor::getTagFromPc(uint32_t pc)
@@ -71,12 +91,14 @@ uint32_t branchPredictor::getTagFromPc(uint32_t pc)
 	return pc >> ( 2 + (int)log2(btbSize) ); /*2 is due to 2 zeroes in 2 lsb bits*/
 }
 
-int branchPredictor::getTagIdx(uint32_t tag) /*return -1 if tag doesnt exist else returns tagIdx*/
+int branchPredictor::getTagIdx(uint32_t pc , uint32_t tag) /*return -1 if tag doesnt exist else returns tagIdx*/
 {
-	uint32_t it = find(tag_vector.begin(),tag_vector.end(), tag);
-	if (it != tag_vector.end())
+	uint32_t mask = pow(2,log2(btbSize)) - 1;
+	pc >>= 2;
+	int idx = pc & mask;
+	if ( tag_vector[idx]==tag )
 	{
-		return ( it-tag_vector.begin() );
+		return idx;
 	}
 	return -1;
 	
@@ -142,18 +164,53 @@ void branchPredictor::updateFsm(uint32_t pc, int tagIdx, bool taken)
 	}
 }
 
-void branchPredictor::insertNewBranch(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst)
+void branchPredictor::insertNewBranch(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst, int tagIdx, uint32_t tag)
 {
-	
+	/*replacement or inserting*/
+	tag_vector[tagIdx] = tag;
+	target_vector[tagIdx] = targetPc;
+	if ( !isGlobalHist )
+	{
+		history_vector[tagIdx] = 0;
+	}
+	if ( !isGlobalTable )
+	{
+		fsm_table[tagIdx][history_vector[tagIdx]] = fsmState;
+	}
+	updateHistory(tagIdx, taken);
+	updateFsm(pc, tagIdx, taken);	
 }
 
+bool branchPredictor::isTaken(uint32_t pc, int tagIdx)
+{
+	int fsmState;
+	if ( isGlobalTable && isGlobalHist)  
+	{
+		uint32_t sharedHistory = getSharedHistory(pc, global_history); //depends on isShare
+		fsmState = global_fsm_table[ sharedHistory ];			
+	}
+	else if ( isGlobalTable && !isGlobalHist ) 
+	{
+		uint32_t sharedHistory = getSharedHistory(pc, history_vector[tagIdx]); //depends on isShare
+		fsmState = global_fsm_table[ sharedHistory ];		
+	}
+	else if ( !isGlobalTable && isGlobalHist )
+	{
+		fsmState = fsm_table[tagIdx][ global_history ];
+	}
+	else if ( !isGlobalTable && !isGlobalHist )
+	{
+		fsmState = fsm_table[tagIdx][ history_vector[tagIdx] ];
+	}
+	return ( fsmState==2 || fsmState==3 );
+}
 
 bool branchPredictor::predict(uint32_t pc, uint32_t *dst)
 {
 	uint32_t tag = getTagFromPc(pc);
-	int tagIdx = getTagIdx(tag);
+	int tagIdx = getTagIdx(pc, tag);
 	
-	if( (tagIdx!=-1) && isTaken(tag) ) 
+	if( (tagIdx!=-1) && isTaken(pc, tagIdx) ) 
 	{
 		*dst = getTargetFromTagIdx(tagIdx);
 		return true;
@@ -170,16 +227,16 @@ void branchPredictor::update(uint32_t pc, uint32_t targetPc, bool taken, uint32_
 {
 	predict(pc, &pred_dst);
 	uint32_t tag = getTagFromPc(pc);
-	int tagIdx = getTagIdx(tag);
-	if( tagIdx != -1 ) /*pc exist in btb*/
+	int tagIdx = getTagIdx(pc, tag);
+	if( tagIdx != -1 ) /*pc exist in btb*/ 
 	{
 		updateTarget(tagIdx, targetPc);
 		updateHistory(tagIdx, taken);
 		updateFsm(pc, tagIdx, taken);
 	}
-	else /*pc does not exist in btb*/
+	else /*pc does not exist in btb: may have to replace or insert*/
 	{
-		insertNewBranch(pc, targetPc, taken, pred_dst);
+		insertNewBranch(pc, targetPc, taken, pred_dst, tagIdx, tag);
 	}
 }
 
@@ -190,7 +247,7 @@ void branchPredictor::update(uint32_t pc, uint32_t targetPc, bool taken, uint32_
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-branchPredictor bp();
+branchPredictor bp(0,0,0,0,0,0,0);
 
 
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,

ef63521147ace1728a9631bc015ff2db50540bfc
doraviv02 <doraviv02@gmail.com>
2023-05-08 10:51:59 +0000

    table size fix, still wrong branches

--- bp.cpp
+++ bp.cpp
@@ -95,6 +95,7 @@ uint32_t branchPredictor::getSharedHistory(uint32_t pc, uint32_t history)
 uint32_t branchPredictor::getTagFromPc(uint32_t pc)
 {
 	return pc >> ( 2 + (int)log2(btbSize_) ); /*2 is due to 2 zeroes in 2 lsb bits*/
+
 }
 
 int branchPredictor::getTagIdx(uint32_t pc , uint32_t tag) /*return -1 if tag doesnt exist else returns tagIdx*/
@@ -133,44 +134,32 @@ void branchPredictor::updateHistory( int tagIdx, bool taken)
 
 void branchPredictor::updateFsm(uint32_t pc, int tagIdx, bool taken)
 {
-	int State;
-	if ( isGlobalTable_ && isGlobalHist_)  
-	{
-		uint32_t sharedHistory = getSharedHistory(pc, global_history_); //depends on isShare
-		State = global_fsm_table_[ sharedHistory ];
-		if ( State==1 || State==2 )  { global_fsm_table_[ sharedHistory ] += (taken?1:-1); }
-		else if ( State==0 ) { global_fsm_table_[ sharedHistory ] += (taken?1:0); }
-		else if ( State==3 ) { global_fsm_table_[ sharedHistory ] += (taken?0:-1); }			
-		
+	int State,change=0;
+	uint32_t sharedHistory;
+	if ( isGlobalTable_ && isGlobalHist_) State = global_fsm_table_[ sharedHistory ];
+
+	else if ( isGlobalTable_ && !isGlobalHist_ ) State = global_fsm_table_[ sharedHistory ];
+
+	else if ( !isGlobalTable_ && isGlobalHist_ ) State = fsm_table_[tagIdx][ global_history_ ];
+
+	else if ( !isGlobalTable_ && !isGlobalHist_ ) State = fsm_table_[tagIdx][ history_vector_[tagIdx] ];
+	if (taken){
+		if (State<3) change =1;
 	}
-	else if ( isGlobalTable_ && !isGlobalHist_ ) 
-	{
-		uint32_t sharedHistory = getSharedHistory(pc, history_vector_[tagIdx]); //depends on isShare
-		State = global_fsm_table_[ sharedHistory ];
-		if ( State==1 || State==2 )  { global_fsm_table_[ sharedHistory ] += (taken?1:-1); }
-		else if ( State==0 ) { global_fsm_table_[ sharedHistory ] += (taken?1:0); }
-		else if ( State==3 ) { global_fsm_table_[ sharedHistory ] += (taken?0:-1); }		
-		
+	else if(State>0) change = -1;
+
+	if ( isGlobalTable_ && isGlobalHist_){
+		global_fsm_table_[getSharedHistory(pc, global_history_)]+=change;
 	}
-	else if ( !isGlobalTable_ && isGlobalHist_ )
-	{
-		State = fsm_table_[tagIdx][ global_history_ ];
-		if ( State==1 || State==2 )  { fsm_table_[tagIdx][ global_history_ ] += (taken?1:-1); }
-		else if ( State==0 ) { fsm_table_[tagIdx][ global_history_ ] += (taken?1:0); }
-		else if ( State==3 ) { fsm_table_[tagIdx][ global_history_ ] += (taken?0:-1); }
+	else if ( isGlobalTable_ && !isGlobalHist_ ) {
+		global_fsm_table_[ getSharedHistory(pc, history_vector_[tagIdx])]+=change;
 	}
-	else if ( !isGlobalTable_ && !isGlobalHist_ )
-	{
-		State = fsm_table_[tagIdx][ history_vector_[tagIdx] ];
-		if ( State==1 || State==2 )  { fsm_table_[tagIdx][ history_vector_[tagIdx] ] += (taken?1:-1); }
-		else if ( State==0 ) { fsm_table_[tagIdx][ history_vector_[tagIdx] ] += (taken?1:0); }
-		else if ( State==3 ) { fsm_table_[tagIdx][ history_vector_[tagIdx] ] += (taken?0:-1); }
+	else if ( !isGlobalTable_ && isGlobalHist_ ){
+		fsm_table_[tagIdx][ global_history_ ]+= change;
 	}
-	/*updating stats_*/
-	
-	//if ( (State==0 || State==1)&&(fsmState_==2 || fsmState_==3) ) { stats_.flush_num++; }
-	//if ( (State==2 || State==3)&&(fsmState_==0 || fsmState_==1) ) { stats_.flush_num++; }
-	
+	else if ( !isGlobalTable_ && !isGlobalHist_ ){
+		fsm_table_[tagIdx][ history_vector_[tagIdx] ]+=change;
+	}
 }
 
 void branchPredictor::insertNewBranch(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst, int tagIdx, uint32_t tag)
@@ -179,13 +168,11 @@ void branchPredictor::insertNewBranch(uint32_t pc, uint32_t targetPc, bool taken
 	tag_vector_[tagIdx] = tag;
 	target_vector_[tagIdx] = targetPc;
 	if ( !isGlobalHist_ )
-	{
 		history_vector_[tagIdx] = 0;
-	}
+
 	if ( !isGlobalTable_ && !isGlobalHist_ )
-	{
 		fsm_table_[tagIdx][ history_vector_[tagIdx] ] = fsmState_;
-	}
+
 	updateFsm(pc, tagIdx, taken);
 	updateHistory(tagIdx, taken);
 }
@@ -201,16 +188,15 @@ bool branchPredictor::isTaken(uint32_t pc, int tagIdx)
 	else if ( isGlobalTable_ && !isGlobalHist_ ) 
 	{
 		uint32_t sharedHistory = getSharedHistory(pc, history_vector_[tagIdx]); //depends on isShare
+		//uint32_t sharedHistory = history_vector_[tagIdx];
 		State = global_fsm_table_[ sharedHistory ];		
 	}
 	else if ( !isGlobalTable_ && isGlobalHist_ )
-	{
 		State = fsm_table_[tagIdx][ global_history_ ];
-	}
+	
 	else if ( !isGlobalTable_ && !isGlobalHist_ )
-	{
 		State = fsm_table_[tagIdx][ history_vector_[tagIdx] ];
-	}
+	
 	return ( State==2 || State==3 );
 }
 
@@ -237,9 +223,8 @@ void branchPredictor::update(uint32_t pc, uint32_t targetPc, bool taken, uint32_
 	stats_.br_num++; // New Branch, Taken or not
 	predict(pc, &pred_dst); // saved prediction in pred_dst
 
-	if ((taken && (pred_dst!=targetPc))|| (!taken && (pred_dst != pc+4))){ //Finding when we need to flush
+	if ((taken && (pred_dst!=targetPc))|| (!taken && (pred_dst != pc+4))) //Finding when we need to flush
 		stats_.flush_num++;
-	}
 
 	uint32_t tag = getTagFromPc(pc);
 	int tagIdx = getTagIdx(pc, tag); //getting tag ID pc from btb
@@ -250,9 +235,8 @@ void branchPredictor::update(uint32_t pc, uint32_t targetPc, bool taken, uint32_
 		updateHistory(tagIdx, taken);
 	}
 	else /*pc does not exist in btb: may have to replace or insert*/
-	{
 		insertNewBranch(pc, targetPc, taken, pred_dst, tagIdx, tag);
-	}
+
 }
 
 unsigned branchPredictor::getFlushNum()
@@ -270,21 +254,19 @@ unsigned branchPredictor::getBtbSize()
 	unsigned fsmSize = pow(2,historySize) * 2;
 	unsigned validBitSize = 1;
 	stats_.size = btbSize_*(validBitSize+targetSize+tagSize_);
-	if ( isGlobalTable_ && isGlobalHist_)  
-	{
-	 	stats_.size += historySize_+fsmSize;
+
+	if(isGlobalHist_){
+		stats_.size += historySize_;
 	}
-	else if ( isGlobalTable_ && !isGlobalHist_ ) 
-	{
-		stats_.size += historySize_+btbSize_*fsmSize;
+	else{
+		stats_.size+= btbSize_*historySize_;
 	}
-	else if ( !isGlobalTable_ && isGlobalHist_ )
-	{
-		stats_.size += btbSize_*historySize_+fsmSize;
+	
+	if (isGlobalTable_){
+		stats_.size+= fsmSize;
 	}
-	else if ( !isGlobalTable_ && !isGlobalHist_ )
-	{
-		stats_.size += btbSize_*(historySize_+fsmSize);
+	else{
+		stats_.size+= btbSize_*fsmSize;
 	}
 	return stats_.size;
 }
@@ -319,4 +301,3 @@ void BP_GetStats(SIM_stats *curStats){
 	curStats->flush_num = bp.getFlushNum();
 	return;
 }
-

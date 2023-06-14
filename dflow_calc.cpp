3feb8247d830778719fb1a4bd57140654c3bb341
doraviv02 <doraviv02@gmail.com>
2023-06-14 11:25:05 +0300

    fixed

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -13,13 +13,11 @@ using namespace std;
 
 class Prog{
     vector<vector<int>> dependencies_vec; // each inst dependecies
-    vector<int> not_exit_vec; // 0-means no inst that depends on inst i
+    vector<unsigned int> depth;
     int reg_arr[REGS_NUM]; // latest inst that modefied reg i 
     unsigned int *opsLatency_;
     unsigned int numOfInsts_;
     InstInfo *progTrace_;
-    int entry_idx;
-    int exit_idx;
 
     public:
         Prog() = default;
@@ -36,29 +34,24 @@ Prog::Prog(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned
     opsLatency_ = (unsigned int*)opsLatency; // forbiddin to change the content of these 2 arrays
     progTrace_ = (InstInfo*)progTrace;
     numOfInsts_ = numOfInsts;
-    not_exit_vec = vector<int>(numOfInsts_, 0); 
+    depth = vector<unsigned int>(numOfInsts_,0);
     dependencies_vec = vector<vector<int>>(numOfInsts_, vector<int>(MAX_DEP_NUM, ENTRY)); // each inst depend on 2 inst max
-    entry_idx = 0; // first inst in the trace is the entry 
-    exit_idx = -1;
     // each inst at first points to entry
-    for(unsigned int i=0; i<numOfInsts_; i++)
-    {
-        dependencies_vec[i] = vector<int>(MAX_DEP_NUM, ENTRY);
-    }
-    for(unsigned int i=0; i<REGS_NUM; i++)
-    {
-        reg_arr[i] = -1; // means at begin, no insts that changed any register
-    }
+    for(unsigned int i=0; i<numOfInsts_; i++){
+        dependencies_vec[i] = vector<int>(MAX_DEP_NUM, ENTRY);}
+
+    for(unsigned int i=0; i<REGS_NUM; i++){
+        reg_arr[i] = -1;} // means at begin, no insts that changed any register
+
     
 }
 
 void Prog::myAnalyzeProg()
 {
     int dst;
-    unsigned int src1;
-    unsigned int src2;
-    int LIidx1; // latest inst idx that changed src1
-    int LIidx2; // latest inst idx that changed src2
+    unsigned int src1, src2;
+    int LIidx1, LIidx2; // latest inst idx that changed srcs
+    unsigned int depth1, depth2; //dependency's depths
     for(unsigned int i=0; i<numOfInsts_; i++)
     {
         dst = progTrace_[i].dstIdx;
@@ -66,76 +59,26 @@ void Prog::myAnalyzeProg()
         src2 = progTrace_[i].src2Idx;
         LIidx1 = reg_arr[src1];
         LIidx2 = reg_arr[src2];
+        depth1 = 0,depth2=0;
         //check "distance"
         if(LIidx1 != -1)
-        {
-            //means inst number i depends on inst number LIidx1;
+        {//means inst number i depends on inst number LIidx1;
             dependencies_vec[i][0] = LIidx1;
-            not_exit_vec[LIidx1] = 1; // inst LIidx1 cant be exit
+            depth1=depth[LIidx1]+opsLatency_[progTrace_[LIidx1].opcode];
         }
         if(LIidx2 != -1)
-        {
-            //means inst number i depends on inst number LIidx2;
+        {//means inst number i depends on inst number LIidx2;
             dependencies_vec[i][1] = LIidx2;
-            not_exit_vec[LIidx2] = 1; // inst LIidx2 cant be exit
+            depth2 = depth[LIidx2]+opsLatency_[progTrace_[LIidx2].opcode];;
         }
         reg_arr[dst] = i; // mark that inst number i changed reg dst
+        depth[i] = max(depth1,depth2);
     }
-    //get exit_idx;
-    for(unsigned int i=0; i<numOfInsts_; i++)
-    {
-        if(not_exit_vec[i] == 0)
-        {
-            exit_idx = i;
-            break;
-        }
-    } 
 }
 
 int Prog::myGetInstDepth(unsigned int instIdx)
 {
-    int depthToReturn = 0;
-    int dependency_idx1;
-    int dependency_idx2;
-    int dep_latency1;
-    int dep_latency2;
-    if(instIdx<0 || instIdx>numOfInsts_) return -1; // invalid inst index
-    dependency_idx1 = dependencies_vec[instIdx][0];
-    dependency_idx2 = dependencies_vec[instIdx][1];
-    while(dependency_idx1 != ENTRY || dependency_idx2 != ENTRY)//till not arrive to entry
-    {
-        if(dependency_idx1 == ENTRY)
-        {
-            // dependency_idx2 != ENTRY
-            dep_latency2 = opsLatency_[progTrace_[dependency_idx2].opcode];
-            depthToReturn += dep_latency2;
-            instIdx = dependency_idx2;
-            dependency_idx1 = dependencies_vec[instIdx][0];
-            dependency_idx2 = dependencies_vec[instIdx][1];
-            continue;
-        }
-        else if(dependency_idx2 == ENTRY)
-        {
-            //dependency_idx1 != ENTRY and dependency_idx2 == ENTRY
-            dep_latency1 = opsLatency_[progTrace_[dependency_idx1].opcode];
-            depthToReturn += dep_latency1;
-            instIdx = dependency_idx1;
-            dependency_idx1 = dependencies_vec[instIdx][0];
-            dependency_idx2 = dependencies_vec[instIdx][1];
-            continue;
-        }
-        else
-        {
-            //dependency_idx1 != ENTRY and dependency_idx2 != ENTRY
-            dep_latency1 = opsLatency_[progTrace_[dependency_idx1].opcode];
-            dep_latency2 = opsLatency_[progTrace_[dependency_idx2].opcode];
-            depthToReturn += ((dep_latency1 > dep_latency2) ? dep_latency1 : dep_latency2);
-            instIdx = ((dep_latency1 > dep_latency2) ? dependency_idx1 : dependency_idx2);
-            dependency_idx1 = dependencies_vec[instIdx][0];
-            dependency_idx2 = dependencies_vec[instIdx][1];
-        }
-    }
-    return depthToReturn;
+    return depth[instIdx];
 }
 
 int Prog::myGetInstDeps(unsigned int instIdx, int *src1DepInst, int *src2DepInst)
@@ -148,20 +91,13 @@ int Prog::myGetInstDeps(unsigned int instIdx, int *src1DepInst, int *src2DepInst
 
 int Prog::myGetProgDepth()
 {
-    int max_prog_depth = 0;
-    int prog_depth = 0;
-    for(unsigned int i=0; i<not_exit_vec.size(); i++)
+    unsigned int max_prog_depth = 0;
+    for (unsigned i = 0; i < numOfInsts_; i++)
     {
-        if(not_exit_vec[i] == 0)
-        {
-            prog_depth = myGetInstDepth(i);
-            exit_idx = i;
-        }
-            
-        if (prog_depth > max_prog_depth)
-            max_prog_depth = prog_depth;
-    } 
-    return max_prog_depth + opsLatency_[progTrace_[exit_idx].opcode];
+        max_prog_depth = max(max_prog_depth,
+        depth[i]+opsLatency_[progTrace_[i].opcode]);
+    }
+    return (int)max_prog_depth;
 }
 
 ///////////////////////// manage the adt of the analyzer ////////////////////////////////////
@@ -178,17 +114,12 @@ void freeProgCtx(ProgCtx ctx) {
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
     return ((Prog*)ctx) -> myGetInstDepth(theInst);
-    //return -1;
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
     return ((Prog*)ctx) -> myGetInstDeps(theInst, src1DepInst, src2DepInst);
-    //return -1;
 }
 
 int getProgDepth(ProgCtx ctx) {
     return ((Prog*)ctx) -> myGetProgDepth();
-    //return 0;
 }
-
-
